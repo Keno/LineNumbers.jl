dee78d72e022e1c7ed2c741d2bb27bdda77d00df
Keno Fischer <kfischer@college.harvard.edu>
2016-02-02 22:46:15 -0500

    Initial code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using LineNumbers
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+code = "f() = 1 + 2 - 3\n"
+@test SourceFile(code)[1:2] == Vector{UInt8}["f() = 1 + 2 - 3".data, "".data]
+data2 = Vector{UInt8}[copy(code.data),[]]
+LineBreaking(UInt64(1),SourceFile(code),data2)[7] = 'x'
+@test bytestring(data2[1]) == "f() = x + 2 - 3\n"
+@test length(SourceFile("f() = 1 + 2 - 3")) == 1
